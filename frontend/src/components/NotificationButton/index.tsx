091fb16f3b62e72f395b3e4ccc32ef06ca91f2c1
sauloqueiroga <sauloerick@hotmail.com>
2022-09-07 17:55:27 -0300

    Other components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,10 +2,10 @@ import icon from '../../assets/img/notification-icon.svg'
 
 import './styles.css'
 
-function NotificationButton(){
-    return(
-        <div className = "dsmeta-red-btn">
-            <img src={icon} alt="Notificar"/>
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
         </div>
     )
 }
