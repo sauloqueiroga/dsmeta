69a1ccf139f1a964ad9f01ecfc18d881197f268c
sauloqueiroga <sauloerick@hotmail.com>
2022-09-08 14:03:52 -0300

    API test

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsmeta.entities;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
